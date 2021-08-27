9a1cbfbe18f0bc4b94452a1e63bcd6ce87783b0c
sunosuporno <sarkarsuporno36@gmail.com>
2021-08-27 11:15:01 +0530

    learnt useRef

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import classes from "./Input.module.css"
+import React, { useRef, useImperativeHandle } from "react";
+import classes from "./Input.module.css";
 
+const Input = React.forwardRef((props, ref) => {
+  const InputRef = useRef();
 
-const Input = (props) => {
-    return (
-        <div
-        className={`${classes.control} ${
-          props.isValid === false ? classes.invalid : ""
-        }`}
-      >
-        <label htmlFor={props.id}>{props.label}</label>
-        <input
-          type = {props.type}
-          id={props.id}
-          value={props.value}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
-        />
-      </div>
-    )
-}
+  const activate = () => {
+    InputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={InputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
 
 export default Input;
